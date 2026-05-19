ccdaa189a19a9ed97724ccba460cb0e6e6ec7283
mehedi hasan <mehedihasan.j135@gmail.com>
2026-05-19 11:30:49 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();
 
+dotenv.config();
 const PORT = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("hello from server");
 });
+run().catch(console.dir);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
